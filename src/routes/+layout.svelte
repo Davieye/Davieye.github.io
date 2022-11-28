788de31bcfef3a5205e9a4d83b9989275ef242b1
David Lindholm <davlin03@edu.umea.se>
2022-11-28 08:56:19 +0100

    added stuff

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<nav>
+  <a href="/">Hem</a>
+  <a href="/telltale">Telltale</a>
+  <a href="/memory">Memory</a>
+
+  <a
+    target="_blank"
+    href="https://github.com/williamviktorsson/williamviktorsson.github.io"
+    >GitHub</a
+  >
+</nav>
+
+<hr />
+
+<slot />
