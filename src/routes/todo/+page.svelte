2ca23bb6da6bc73cb40eebb303ea58ee80e24748
David Lindholm <davlin03@edu.umea.se>
2022-12-05 09:16:14 +0100

    todo app start

---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,26 @@
+<body>
+ <header> 
+    <h1> Att göra lista :p</h1> 
+    <form id= "ny-task-form">
+        <input
+            type="text"
+            id="ny-task-input"
+            placeholder="Vad har du planerat?"/>
+        <input 
+            type="submit"
+            id="new-task-submit"
+            value="Lägg till" />
+    </form>
+
+
+</header>
+</body> 
+
+
+<script>
+
+</script>
+
+<style>
+
+</style>
