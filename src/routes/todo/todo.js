c462a41b3a9f0d1d36ef6f1213c8cd0347aec00d
David Lindholm <davlin03@edu.umea.se>
2022-12-19 11:17:29 +0100

    tod 8

---
+++ src/routes/todo/todo.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', () =>{
+    const form = document.querySelector("#ny-task-form");
+    const input = document.querySelector("#ny-task-input");
+    const list_el = document.querySelector("#ärende");   
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const ärende = input.value;
+
+        if (!ärende) {
+            alert("Please add a task");
+            return;
+        } 
+        const ärende_el = document.createElement("div");
+        ärende_el.classList.add("ärende");
+
+        const ärende_content_el = document.createElement("div");
+        ärende_content_el.classList.add("kontent");
+        ärende_content_el.innerText = test;
+
+        ärende_el.appendChild(ärende_content_el);
+
+        list_el.appendChild(ärende_el);
+
+    })
+})
