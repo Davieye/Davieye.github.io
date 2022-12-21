dbe6026189b2c00d2d81c429b1f57b3e7df44829
David Lindholm <davlin03@edu.umea.se>
2022-12-21 13:50:16 +0100

    rol3

--- src/lib/stories.js
+++ src/lib/stories.js
@@ -1,6 +1,7 @@
 export let stories = [
      {id:"1",message: "Rollin way to school.", img:"rollin way to school.jpg",choices:[{id:"2",text:"Begin"}]},
     {id:"2",message: "What will be your path?", img:"choice.jpeg",choices:[{id:"3",text:"Left"},{id:"4",text:"Right"}]},
-     {id:"3",message: "Fel håll xd", img:"choice.jpeg",choices:[{id:"5",text:"Other right"},{id:"4",text:"Right"}]},
+     {id:"3",message: "Fel håll xd", img:"choice.jpeg",choices:[{id:"5",text:"Other right"},{id:"6",text:"Right"}]},
      {id:"4",message: "Rollin hitta dit", img: "klassrum.png" ,choices:[{id:"1",text:"Reset!"}]} ,
-     {id:"5",message: "Rollin död :(", img: "påkörd.png" ,choices:[{id:"1",text:"Reset!"}]} ]
+     {id:"5",message: "Rollin död :(", img: "påkörd.png" ,choices:[{id:"1",text:"Reset!"}]} ,
+     {id:"6",message: "Du träffar elis", img: "VEM FAN.png" ,choices:[{id:"3",text:"Gå tillbaka"}]}    ]
