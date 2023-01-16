1243842173d421fb69ce639fa520f19e74a4162b
David Lindholm <davlin03@edu.umea.se>
2023-01-16 08:50:52 +0100

    mm

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
   <a href="/">Hem</a>
   <a href="/telltale">Telltale</a>
   <a href="/memory">Memory</a>
-
+  <a href="/todo">Todo</a>
   <a
     target="_blank"
     href="https://github.com/Davieye/Davieye.github.io"
